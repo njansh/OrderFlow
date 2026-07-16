eb000ab897641cb5bbb31a7f0f39130b1ec83354
Njansh <nadsonjhonyobmep@gmail.com>
2026-07-16 15:00:10 -0300

    feat: implement login

---
+++ src/main/java/com/nadson/orderflow/modules/users/infra/controller/dto/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.nadson.orderflow.modules.users.infra.controller.dto;
+
+public record LoginResponse(String token) {
+}
--- src/main/java/com/nadson/orderflow/shared/security/SecurityConfig.java
+++ src/main/java/com/nadson/orderflow/shared/security/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.nadson.orderflow.shared.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,5 +40,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
